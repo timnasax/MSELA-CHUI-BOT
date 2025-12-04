12221b66d1752d0ec921500488d40a8c8d21a1da
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-12-05 01:01:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -353,7 +353,7 @@ class HybridConfigManager {
 const hybridConfig = new HybridConfigManager();
 module.exports = {
     hybridConfig,
-    session: process.env.SESSION_ID || '',
+    session: process.env.SESSION_ID || 'Bravintech_91zhg',
     sessionId: hybridConfig.getSessionId(),
     PREFIX: process.env.PREFIX || ".",
     GURL: 'https://whatsapp.com/channel/0029Vb7GAAHInlqGmtHEbD2q',
